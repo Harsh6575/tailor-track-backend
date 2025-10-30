6324dc75b01340652f45e27755b11176a6a12ddd
“Harsh6575” <harsh.190410107146@gmail.com>
2025-10-30 11:53:42 +0530

    fix: implemented esconfig newer way

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["dist", "node_modules", ".husky", ".drizzle"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  prettier
+);
